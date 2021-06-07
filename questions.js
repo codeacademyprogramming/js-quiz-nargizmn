d86af0787bc83d7124042a89bcb1bc1cc087765b
unknown <nargizmn@code.edu.az>
2021-06-07 06:01:43 -0700

    42

---
+++ questions.js
@@ -0,0 +1,55 @@
+/*
+    1. Write an expression which selects every <p> element in the document (write in all ways you know)
+*/
+
+/*
+    2. Write an expression which selects every element with the wrapper class in the document (write in all ways you know)
+*/
+
+/*
+    3. You are given an array [1,2,3,4,5,6,7,8,9,10], create a new array based on this one which only contains numbers can divide by two (with built in array methods)
+*/
+
+/*
+    4. You are given an array [1,2,3,4,5,6,7,8,9,10], create a new array based on this one which is multiplied versions of every element in the array(with built in array methods)
+*/
+
+/*
+    5. Write a custom method which accepts an array and returns an array with all the values duplicated
+    input: [1,2,3,4,5]
+    expected output: [1,2,3,4,5,1,2,3,4,5]
+*/
+
+/*
+    6. Write a custom method with name 'multiply' which can be invoked like the example below:
+    multiply(2)(3)(5)
+    expected output: 60
+*/
+
+/*
+    7. [1,2,3,4,5,6,7,8,9,10], check with an array method if the array only contains numbers greater than 5
+*/
+
+/*
+    8. [1,2,3,4,5,6,7,8,9,10], check with an array method if the array at least contains one element which divides by 2
+*/
+
+/*
+    9.  What will be the output of the following code?
+        var Employee = {
+            company: 'xyz'
+        }
+        var emp1 = Object.create(Employee);
+        delete emp1.company
+        console.log(emp1.company);
+*/
+
+/*
+    10. I wrote a function, how can i know how many arguments provided to my function
+*/
+
+/*
+    11. You are given an array like [1,2,3,4], how can you empty array? (write in )
+    input: [1, 2, 3, 4]
+    output: []
+*/
